8062fb911448c8b9ae74eed2c112cdbee269f5b3
Bryan Guerra <bryanmlg2018@gmail.com>
2023-04-10 16:40:42 -0600

    ✅Login working

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,15 +1,24 @@
 import { LockClosedIcon } from '@heroicons/react/solid';
-import Image from 'next/image';
+import { useRef } from 'react';
 export default function LoginPage() {
+  const form = useRef(null);
+  const HandleSubmit = event => {
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password'),
+    };
+    console.log(data);
+  };
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <Image alt="Workflow" className="mx-auto h-12 w-auto" src="https://tailwindui.com/Image/logos/workflow-mark-indigo-600.svg" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-          <form action="#" className="mt-8 space-y-6" method="POST">
+          <form ref={form} action="#" className="mt-8 space-y-6" method="POST">
             <input defaultValue="true" name="remember" type="hidden" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -61,6 +70,7 @@ export default function LoginPage() {
               <button
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 type="submit"
+                onClick={HandleSubmit}
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon aria-hidden="true" className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" />
